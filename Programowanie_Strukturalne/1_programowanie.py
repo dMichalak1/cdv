0f4963a162434d9484fb0c0ce4aafcc95bc7dc17
Daniel Michalak <dmichalak@edu.cdv.pl>
2019-03-25 15:31:39 +0100

    1_programowanie

---
+++ Programowanie_Strukturalne/1_programowanie.py
@@ -0,0 +1,23 @@
+print("CDV")
+#pobieranie danych z kl
+#imie=input()
+
+#print("Twoje imie podane z klawiatury: "+imie)
+#nazwisko=input()
+#print("Twoje nazwisko podane z klawiatury: "+nazwisko)
+
+#dlugosc=len(nazwisko)
+
+#dlugosc=len(dlugosc)
+#print(type(dlugosc))
+#print("DL nazwiska: ",dlugosc)
+
+#dlugosc=str(dlugosc)
+#print(type(dlugosc))
+#print("DL nazwiska: "+dlugosc)
+print("\nPodaj swoj wiek: ", end="")
+wiek = input()
+print(type(wiek)) #string
+print("Twój wiek: ",wiek, "lata")
+
+nazwisko = 'Kowalski'
