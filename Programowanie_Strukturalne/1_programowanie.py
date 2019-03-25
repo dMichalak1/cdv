7a55e87becaca154a87e0a1f2fc34f8564af598c
Daniel Michalak <dmichalak@edu.cdv.pl>
2019-03-25 15:58:41 +0100

    Python

--- Programowanie_Strukturalne/1_programowanie.py
+++ Programowanie_Strukturalne/1_programowanie.py
@@ -21,3 +21,40 @@ print(type(wiek)) #string
 print("Twój wiek: ",wiek, "lata")
 
 nazwisko = 'Kowalski'
+pierwszyZnak = nazwisko[0];
+print(pierwszyZnak) #K
+
+ostatniZnak = nazwisko[len(nazwisko)-1]
+print(ostatniZnak) #i
+
+ostatniZnak = nazwisko[-1]
+print(ostatniZnak)
+
+x = "5"
+print(type(x)) #str
+x = int(x)
+print(type(x)) #int
+
+text = "Jan" * 2
+print(text)
+print(type(text)) #str
+
+y = 6
+print(type(y)) #int
+y = y / 2
+print(type(y)) #flout
+print(y) #3.0
+
+wiek = 21
+print("Twoj wiek: ",wiek)
+wiek=str(wiek)
+print("Twoj wiek: " +wiek)
+
+nazwisko = "Jankowski"
+print(nazwisko[0])   #J
+print(nazwisko[0:3]) #Jan
+print(nazwisko[-2])  #k
+print(nazwisko[-2:]) #ki
+print(nazwisko[:-2]) #Jankows
+print(nazwisko[:-2:2]) #Jnos
+print(nazwisko[::2]) #Jnosi
